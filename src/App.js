05c856a6507eda5f6e92731e1803d4a7d616450a
Tran Giang <trangiang295@gmail.com>
2022-09-06 00:26:45 +0700

    fix route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Test from './pages/Test/test';
+import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
+import Term from './pages/Term/Term';
 import { HOME, LOGIN, TERM } from './global/routes';
 
 const App = () => {
@@ -12,7 +12,7 @@ const App = () => {
       <Routes>
         <Route path={HOME} element={<Home />} />
         <Route path={LOGIN} element={<Login />} />
-        <Route path={TERM} element={<TERM />} />
+        <Route path={TERM} element={<Term />} />
       </Routes>
     </Router>
   );
